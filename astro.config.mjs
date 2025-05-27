90a219b95195bd2bb86853611fdb3bea61c7e9cb
joshie-wu <your-github-email@example.com>
2025-05-26 21:50:57 -0400

    static generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  output: 'static', // This ensures static generation
   site: 'https://joshie-wu.github.io',
-  base: '/personal-website', // Use your repository name
+  base: '/personal-website',
 });
